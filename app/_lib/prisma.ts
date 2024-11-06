76eb3ddb1841f4bb1eedb5263329a9287fdb755f
Renan Victor <renan22rsw@gmail.com>
2024-11-06 20:11:50 -0300

    feat: add transaction type badge

---
+++ app/_lib/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+//codigo responsavél por evitar muitas instâncias
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient(); //abre conexão com o banco de dados
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
