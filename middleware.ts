a639b5051f66643126cbfee8e35e7af22d66bb26
Renan Victor <renan22rsw@gmail.com>
2025-08-27 22:18:09 -0300

    fix: domain

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  authorizedParties: ["https://financeai.dpdns.org"],
+});
 
 export const config = {
   matcher: [
